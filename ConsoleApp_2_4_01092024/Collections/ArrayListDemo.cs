54749261dd6164aa81d77643dedd11d204d893e1
lenovo <lenovo@DESKTOP-G3RV5V6>
2024-10-20 15:48:27 +0530

    Arrays and Array List

---
+++ ConsoleApp_2_4_01092024/Collections/ArrayListDemo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Threading;
+
+namespace ConsoleApp_2_4_01092024.Collections
+{
+    internal class ArrayListDemo
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.Add(9);
+            list.Add(10);
+
+            for (int i = 11; i <= 100; i++)
+            {
+                list.Add(i);
+                list.Add("hii");
+            }
+
+            Console.WriteLine("After adding all values size of array list : " +
+                 list.Count);
+            Console.WriteLine("system in process....");
+            Thread.Sleep(2000);
+
+            for (int i = 51; i <= 100; i++)
+            {
+                list.Remove(i);
+
+            }
+
+            Console.WriteLine("After removing 50 elements, size of array list : " +
+                 list.Count);
+
+            Thread.Sleep(2000);
+
+            foreach (var item in list)
+            {
+                if ((int)item % 2 == 0)
+                    Console.WriteLine(item);
+            }
+
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //int[] list = new int[] { 1,2,3,4,5,6,7,8,9,10};
+
+            //for (int i = 10; i < 100; i++) 
+            //{
+            //    list[i] = i+1;
+            //}
+        }
+    }
+}
---
+++ ConsoleApp_2_4_01092024/Collections/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2_4_01092024.Collections
+{
+    //Input : How are you        => output : you are how.
+    //Input : How are you Nitesh => output : Hwo aer yuo Nhseti.
+    //Input : AAABBBCCCAA        => output : A3B3C3A2.
+    internal class Startup
+    {
+        static void Main(string[] args)
+        {
+            //int[] Evennumbers = new int[] { 2, 4, 6, 8, 10, 11, 13, 15 };
+
+            //for (int i = 0; i < Evennumbers.Length; i++)
+            //{
+            //    if (Evennumbers[i] % 2 != 0)
+            //        Console.WriteLine(Evennumbers[i]);
+            //}
+
+            //Console.WriteLine("-----------------------------------");
+            //string[,] SeatAllocation = new string[2, 2] { { "A1", "A2" }, { "B1", "B2" } };
+            //for (int r = 0; r < 2; r++)
+            //{
+            //    for (int c = 0; c < 2; c++)
+            //    {
+            //        Console.Write(SeatAllocation[c,r] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            int[] EvenNumbers = new int[] { 2, 4, 6, 8, 10,2,2};
+            int[] OddNumbers = new int[] { 1, 3, 5, 7, 9,11,13,15,16 };
+
+            int[][] NumberSystems = new int[][] { EvenNumbers, OddNumbers };
+
+            //for (int i = 0; i < numbersystems.length; i++)
+            //{
+            //    for (int j = 0; j < numbersystems[i].length; j++)
+            //    {
+            //        console.write(numbersystems[i][j] + "\t");
+            //    }
+            //    console.writeline();
+            //}
+
+            foreach (int[] NumsArrays in NumberSystems)
+            {
+                foreach (int item in NumsArrays)
+                {
+                    Console.Write(item + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

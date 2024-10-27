fc6c25d8361917eab12db990378ad744750302a9
lenovo <lenovo@DESKTOP-G3RV5V6>
2024-10-27 16:06:26 +0530

    Generic and Exception

---
+++ ConsoleApp_2_4_01092024/ExceptionHandling/ExceptionHandlingDemo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2_4_01092024.ExceptionHandling
+{
+    internal class ExceptionHandlingDemo
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine();
+
+            //string filePath = "C:\\Users\\lenovo\\source\\repos\\Batch_2_4_01092024\\ConsoleApp_2_4_01092024\\Files\\demo2.txt";
+            ////string ActualPath = 
+
+            ////if (File.Exists(filePath))
+            ////{
+            ////}
+            //Console.WriteLine("Program completed.");
+            //    StreamReader streamReader = new StreamReader(filePath);
+            //    Console.WriteLine(streamReader.ReadToEnd());
+
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //int res = a / b;
+
+            try
+            {
+                //string filePath = "C:\\Users\\lenovo\\source\\repos\\Batch_2_4_01092024\\ConsoleApp_2_4_01092024\\Files\\demo2.txt";
+                // StreamReader streamReader = new StreamReader(filePath);
+                // Console.WriteLine(streamReader.ReadToEnd());
+                Console.Write("Enter N1 : ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter N2 : ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Res : " + (a/b));
+                //
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Number can not be divide by zero.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("system only accept numeric value.");
+            }
+            catch (Exception ex)
+            {
+                string filePath = "C:\\Users\\lenovo\\source\\repos\\Batch_2_4_01092024\\ConsoleApp_2_4_01092024\\Files\\demo.txt";
+                 StreamWriter writer = new StreamWriter(filePath,true);
+                writer.WriteLine("-------------Error : {0}-------------------",DateTime.Now.ToString());
+                writer.WriteLine("Message : " + ex.ToString());
+                writer.WriteLine("Trace : " + ex.StackTrace);
+                writer.WriteLine("---------------------------------");
+                writer.Close();
+
+
+                Console.WriteLine("There is proble while processing this request, system sent this notification to the team they will soon reach out to you.");
+            }
+            finally
+            {
+                Console.WriteLine("Finally bloc invoked.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp_2_4_01092024/GenericCollection/Queue_Stack_Demo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2_4_01092024.GenericCollection
+{
+    internal class Queue_Stack_Demo
+    {
+        static void Main(string[] args)
+        {
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Enqueue(4);
+            //queue.Enqueue(5);
+
+            ////queue.Enqueue(6);
+            //queue.Dequeue();
+            //queue.Dequeue();
+
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            Stack<int> stact = new Stack<int>();
+
+            stact.Push(1);
+            stact.Push(2);
+            stact.Push(3);
+            stact.Push(4);
+
+            Console.WriteLine("------peek------------------");
+            Console.WriteLine(stact.Pop());
+            //stact.Pop();
+            Console.WriteLine("------peek------------------");
+            Console.WriteLine(stact.Pop());
+
+            Console.WriteLine("------peek------------------");
+            Console.WriteLine(stact.Pop());
+
+            Console.WriteLine("------peek------------------");
+            Console.WriteLine(stact.Pop());
+
+            //foreach (var item in stact)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //List<int> list = new List<int>();
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //list.Add(4);
+            //list.Add(5);
+
+            ////list.Insert(0, 6);
+            ////list.Remove(3);
+
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
